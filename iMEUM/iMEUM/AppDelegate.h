65dc1dca5f50539c956a0394413184b9cd350066
LoiWu <superloiwu@gmail.com>
2015-02-11 18:36:06 +0800

    iMEUM

---
+++ iMEUM/iMEUM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMEUM
+//
+//  Created by loi on 2/11/15.
+//  Copyright (c) 2015 GWrabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
